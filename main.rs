a8060eb766cc607f3316953ffd3a6b33ef410fa7
lsc <826216325@qq.com>
2026-09-03 06:28:27 +0800

    start build-redis

---
+++ main.rs
@@ -0,0 +1,55 @@
+use std::io::{self, BufRead, Write};
+
+fn parse_args(line: &str) -> Vec<String> {
+    let mut args = Vec::new();
+    let mut current = String::new();
+    let mut in_quotes = false;
+    for ch in line.chars() {
+        match ch {
+            '"' if !in_quotes => in_quotes = true,
+            '"' if in_quotes => in_quotes = false,
+            ' ' if !in_quotes => {
+                if !current.is_empty() {
+                    args.push(current.clone());
+                    current.clear();
+                }
+            }
+            _ => current.push(ch),
+        }
+    }
+    if !current.is_empty() { args.push(current); }
+    args
+}
+
+fn encode_bulk_string(s: &str) -> String {
+    format!("${}\r\n{}\r\n", s.len(), s)
+}
+
+fn handle_command(args: &[String]) -> String {
+    let cmd = args[0].to_uppercase();
+
+    match cmd.as_str() {
+        "PING" => {
+            // TODO: Return "+PONG\r\n" for no args
+            // TODO: Return bulk string for PING <message>
+            String::new()
+        }
+        _ => format!("-ERR unknown command\r\n"),
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let mut out = stdout.lock();
+
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let line = line.trim().to_string();
+        if line.is_empty() { continue; }
+        let args = parse_args(&line);
+        let response = handle_command(&args);
+        write!(out, "{}", response).unwrap();
+        out.flush().unwrap();
+    }
+}
