43b7d1bf4cf89eb56f29edb5fea2bb0b4977c8cb
lsc <826216325@qq.com>
2026-09-04 21:45:37 +0800

    done

--- main.rs
+++ main.rs
@@ -43,27 +43,78 @@ fn ei(n: i64) -> String {
     format!(":{}\r\n", n)
 }
 
-fn handle_command(args: &[String]) -> String {
-    let cmd = args[0].to_uppercase();
+#[derive(Debug)]
+enum Command {
+    Ping,
+    Echo,
+    Command,
+    Unknown,
+}
 
-    match cmd.as_str() {
-        "PING" => {
+impl Command {
+    pub fn from_str(s: &str) -> Result<Self, String> {
+        match s.to_uppercase().as_str() {
+            "PING" => Ok(Command::Ping),
+            "ECHO" => Ok(Command::Echo),
+            "COMMAND" => Ok(Command::Command),
+            _ => Err(format!("ERR unknown command '{}'", s)),
+        }
+    }
+
+    pub fn arity(&self) -> Option<(usize, Option<usize>)> {
+        match self {
+            Command::Ping => Some((0, Some(1))),
+            Command::Echo => Some((1, Some(1))),
+            Command::Command => Some((0, Some(0))),
+            Command::Unknown => None,
+        }
+    }
+
+    pub fn check_arity(&self, args_len: usize) -> Result<(), String> {
+        let name = format!("{:?}", self).to_uppercase();
+        if let Some((min, max)) = self.arity() {
+            if args_len < min {
+                return Err(format!(
+                    "ERR wrong number of arguments for '{}' command",
+                    name
+                ));
+            }
+            if let Some(max) = max {
+                if args_len > max {
+                    return Err(format!(
+                        "ERR wrong number of arguments for '{}' command",
+                        name
+                    ));
+                }
+            }
+            Ok(())
+        } else {
+            Err(format!("ERR unknown command {}", name))
+        }
+    }
+}
+
+fn handle_command(args: &[String]) -> String {
+    let cmd = match Command::from_str(&args[0]) {
+        Ok(cmd) => cmd,
+        Err(err) => return ee(&err),
+    };
+
+    if let Err(err) = cmd.check_arity(args.len() - 1) {
+        return ee(&err);
+    }
+
+    match cmd {
+        Command::Ping => {
             if let Some(msg) = args.get(1) {
                 eb(Some(msg))
             } else {
                 es("PONG")
             }
         }
-        "ECHO" => {
-            if let Some(msg) = args.get(1) {
-                eb(Some(msg))
-            } else {
-                "-ERR wrong number of arguments for 'ECHO'\r\n".to_string()
-            }
-        }
-        "COMMAND" => es("OK"),
-        // _ => format!("-ERR unknown command\r\n"),
-        _ => ee(&format!("ERR unknown command '{}'", cmd)),
+        Command::Echo => eb(Some(args[1].as_str())),
+        Command::Command => es("OK"),
+        Command::Unknown => unreachable!(),
     }
 }
 
